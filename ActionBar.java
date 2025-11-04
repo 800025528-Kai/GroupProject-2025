d09c8e21e94d86eed52cecbb78a210ac550c5dcd
Leo Zhang <leoz7758258@gmail.com>
2025-11-04 15:30:12 -0800

    improved ActionBar

--- ActionBar.java
+++ ActionBar.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class ActionBar {
+    private ArrayList<Character> actionOrder;
+    private ArrayList<Double> actionValue;
+    // each index corresponds to each other
+    // e.g. actionOrder.get(0) has actionValue.get(0) as its action value
+
+    public ActionBar() {
+        this.actionOrder = new ArrayList<>();
+        this.actionValue = new ArrayList<>();
+    }
+
+    public void addCharacter(Character c, double value) {
+        this.actionOrder.add(c);
+        this.actionValue.add(value);
+    }
 }
